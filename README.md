72867c04e9e69e1d752918ce9ebc4fb329333cbe
Muteeb Akram <muteebakram@gmail.com>
2024-12-06 18:16:22 -0700

    Complated Lab 11 Bluetooth activties - temp service, logging, advertising

--- README.md
+++ README.md
@@ -2,3 +2,48 @@
 
 **Authors**: Jared & Muteeb
 
+## Activity - advertising
+
+- Changed the BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME to 'Muteeb BLE'.
+- Serial port prints:
+
+```
+BTstack counter 0001
+BTstack up and running on 28:CD:C1:0F:20:C7.
+```
+
+**Muteeb BLE Advertising**
+
+![Muteeb BLE Advertising](./imgs/lab11-bluetooth-activity-adversting.png)
+
+## Activity - attributes, services, and characteristics.
+
+### Task 1
+
+| ![Muteeb BLE Services](./imgs/lab11-bluetooth-activity-services-1.png) | ![Muteeb BLE Battery Service Properties](./imgs/lab11-bluetooth-activity-services-2.png) |
+| ---------------------------------------------------------------------- | ---------------------------------------------------------------------------------------- |
+| Muteeb BLE Services                                                    | Muteeb BLE Battery Service Properties                                                    |
+
+### Task 2
+
+| ![Muteeb BLE Gatt Services Name Change](./imgs/lab11-bluetooth-activity-services-3.png) |
+| --------------------------------------------------------------------------------------- |
+| Muteeb BLE Gatt Services Name Change                                                    |
+
+### Task 3
+
+| ![The characteristic has type GAP_DEVICE_NAME, is read only, and includes a literal string value.](./imgs/lab11-bluetooth-activity-services-4.jpeg) |
+| --------------------------------------------------------------------------------------------------------------------------------------------------- |
+| The characteristic has type GAP_DEVICE_NAME, is read only, and includes a literal string value.                                                     |
+
+## Activity 3 - HCI packet handling
+
+Improved our debugging by logging whenever a client connects to our device.
+
+![Muteeb BLE Logging](./imgs/lab11-bluetooth-activity-logging.png)
+
+## Activity 4 - add a service
+
+Temperature Service
+
+![Muteeb BLE Temperature Service](./imgs/lab11-bluetooth-activity-temperature.png)
--- server/picow_bt_example_freertos.c
+++ server/picow_bt_example_freertos.c
@@ -17,25 +17,45 @@
 #define RUN_FREERTOS_ON_CORE 0
 #endif
 
-#define TEST_TASK_PRIORITY				( tskIDLE_PRIORITY + 2UL )
-#define BLINK_TASK_PRIORITY				( tskIDLE_PRIORITY + 1UL )
+#define TEST_TASK_PRIORITY (tskIDLE_PRIORITY + 2UL)
+#define BLINK_TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)
 
-int btstack_main(int argc, const char * argv[]);
+// Global variable to store number of connections
+uint8_t connections = 1;
+
+int btstack_main(int argc, const char *argv[]);
 static btstack_packet_callback_registration_t hci_event_callback_registration;
 
-static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
+static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size)
+{
     UNUSED(size);
     UNUSED(channel);
     bd_addr_t local_addr;
-    if (packet_type != HCI_EVENT_PACKET) return;
-    switch(hci_event_packet_get_type(packet)){
-        case BTSTACK_EVENT_STATE:
-            if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) return;
-            gap_local_bd_addr(local_addr);
-            printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
-            break;
-        default:
-            break;
+    if (packet_type != HCI_EVENT_PACKET)
+        return;
+    switch (hci_event_packet_get_type(packet))
+    {
+    case BTSTACK_EVENT_STATE:
+        if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING)
+            return;
+        gap_local_bd_addr(local_addr);
+        printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
+        break;
+
+    // Logs number of connections
+    case BTSTACK_EVENT_NR_CONNECTIONS_CHANGED:
+        if (btstack_event_nr_connections_changed_get_number_connections(packet))
+        {
+            printf("Number of devices connected since the BLE transmission %d\n", connections);
+        }
+        else
+        {
+            printf("Device %d Disconnected\n", connections);
+            connections++;
+        }
+        break;
+    default:
+        break;
     }
 }
 
@@ -43,16 +63,20 @@ void main_task(__unused void *params)
 {
     // initialize CYW43 driver architecture
     // (will enable BT if/because CYW43_ENABLE_BLUETOOTH == 1)
-    if (cyw43_arch_init()) {
+    if (cyw43_arch_init())
+    {
         printf("failed to initialise cyw43_arch\n");
-    } else {
+    }
+    else
+    {
         // inform about BTstack state
         hci_event_callback_registration.callback = &packet_handler;
         hci_add_event_handler(&hci_event_callback_registration);
         btstack_main(0, NULL);
     }
 
-    while(true) {
+    while (true)
+    {
         vTaskDelay(1000);
     }
 }
